db6b6ee91f297bbd100b18acef4ba3312e5a5408
Abhishek <48253280+abhiLinkd@users.noreply.github.com>
2019-04-23 02:18:39 +0530

    Update MyStoppingThreadExample.java

--- 2-b-StoppingThreads/src/main/MyStoppingThreadExample.java
+++ 2-b-StoppingThreads/src/main/MyStoppingThreadExample.java
@@ -4,6 +4,7 @@ public class MyStoppingThreadExample {
 
 	public static void main(String[] args) {
 		
+		//https://codepumpkin.com/interrupt-interrupted-isinterrupted-java-multithreading/
 		// Source : https://www.youtube.com/watch?v=_RSAS-gIjGo
 		
 	}
